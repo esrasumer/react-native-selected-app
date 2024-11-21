1d2040df53cb5069241b49c55e9f925822200006
esrasumer <esrasumer28@gmail.com>
2024-11-22 00:52:50 +0300

    added new api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,40 @@
-import React, { useState } from "react";
-import { View, TextInput, FlatList, Text, Image, StyleSheet } from "react-native";
-import { useQuery } from "@tanstack/react-query";
-import { fetchCharacters } from "../../src/api/rickAndMortyApi";
+import { View, StyleSheet } from "react-native";
+import { MultipleSelectList } from "react-native-dropdown-select-list";
+import React from "react";
 
 export default function HomeScreen() {
-  const [searchQuery, setSearchQuery] = useState("");
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["characters", searchQuery],
-    queryFn: () => fetchCharacters(searchQuery),
-    enabled: !!searchQuery, 
-  });
+  const [selected, setSelected] = React.useState([]);
+
+  const data = [
+    { key: '1', value: 'Mobiles', disabled: true },
+    { key: '2', value: 'Grocery' },
+    { key: '3', value: 'cameras' },
+    { key: '4', value: 'Computers', disabled: true },
+    { key: '5', value: 'Food' },
+    { key: '6', value: 'Fashion' },
+    { key: '7', value: 'Drinks' },
+  ]
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      <TextInput
-        placeholder="Search for characters"
-        style={styles.input}
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-      />
-
-      {isLoading && <Text>Loading...</Text>}
-      {isError && <Text>Error fetching characters</Text>}
-
-      <FlatList
+    <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: 20 }}>
+      <MultipleSelectList
+        setSelected={(val) => setSelected(val)}
         data={data}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.characterCard}>
-            <Image source={{ uri: item.image }} style={styles.characterImage} />
-            <View>
-              <Text style={styles.characterName}>{item.name}</Text>
-              <Text>Episodes: {item.episode.length}</Text>
-            </View>
-          </View>
-        )}
+        label="Categories"
+        onSelect={() => console.log(selected)}
+        save="value"
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
   },
-  characterCard: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    backgroundColor: "#f9f9f9",
-    borderRadius: 5,
-    padding: 10,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-    elevation: 3,
-  },
-  characterImage: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
-    borderRadius: 25,
-  },
-  characterName: {
-    fontWeight: "bold",
-    fontSize: 16,
-  },
+
 });
